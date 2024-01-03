a746fbcf5e37c57286e2613c16c805e6a74f1780
leventozz <leventufukozturk@gmail.com>
2024-01-03 20:19:03 +0300

    added resultlist comp

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,14 +2,25 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
+import ResultList from '../components/ResultList';
 
 
 
 export default function SearchScreen() {
-    const [searchApi,results] = useResults();
+    const [searchApi, results] = useResults();
+
+    const filterResultByPrice = (price) => {
+        return results.filter((result) => {
+            return result.price === price
+        })
+    }
+
     return (
         <View>
             <SearchBar />
+            <ResultList title='Cheap Restaurants' results={filterResultByPrice('₺')} />
+            <ResultList title='Affordable Restaurants' results={filterResultByPrice('₺₺')} />
+            <ResultList title='Expensive Restaurants' results={filterResultByPrice('₺₺₺')} />
         </View>
     )
 }
