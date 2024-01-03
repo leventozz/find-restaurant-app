fa7cae7b700226a6120bf07c90c0f4f340dd3ec7
leventozz <leventufukozturk@gmail.com>
2024-01-03 17:29:00 +0300

    added search component

---
+++ components/SearchBar.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+
+export default function SearchBar() {
+  return (
+    <View style={styles.backgroundStyle}>
+      <AntDesign style={styles.iconStyle} name="search1" size={30} color="black" />
+      <TextInput style={styles.inputStyle} placeholder='Search' autoCapitalize='none' autoCorrect={false}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  backgroundStyle:{
+    backgroundColor:'lightgray',
+    flexDirection:'row',
+    margin:10,
+    height:50,
+    alignItems:'center',
+    borderRadius:25
+  },
+  iconStyle:{
+    marginHorizontal:10
+  },
+  inputStyle:{
+    flex:1,
+    fontSize:18
+  }
+})
