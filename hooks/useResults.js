0012280180711c7cd7f6fff9d18637afc0dce522
leventozz <leventufukozturk@gmail.com>
2024-01-03 18:19:55 +0300

    added hook

---
+++ hooks/useResults.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+const useResults = () => {
+    const [results, setResults] = useState([]);
+
+    const searchApi = async (searchTerm) => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: searchTerm,
+                location: 'İstanbul',
+            },
+        });
+        setResults(response.data.businesses);
+    };
+
+    useEffect(() => {
+        searchApi('Toast');
+    }, []);
+
+    return [searchApi, results];
+};
+
+export default useResults;
+
+// export default () => {
+
+//     const searchApi = async (searchTerm) => {
+//         const [results, setResults] = useState([]);
+//         const response = await yelp.get('/search', {
+//             params: {
+//                 limit: 50,
+//                 term: searchTerm,
+//                 location: 'İstanbul'
+//             }
+//         });
+//         setResults(response)
+//     }
+
+//     useEffect(() => {
+//         searchApi('Toast');
+//     }, [])
+
+//     return [searchApi, results];
+// }
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import SearchBar from '../components/SearchBar'
+import useResults from '../hooks/useResults'
+
 
 
 export default function SearchScreen() {
+    const [searchApi,results] = useResults();
     return (
         <View>
             <SearchBar />
