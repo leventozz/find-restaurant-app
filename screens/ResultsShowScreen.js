fbb18b10d5557e28eaf9dafe372c7f9c62367b61
leventozz <leventufukozturk@gmail.com>
2024-01-04 10:15:37 +0300

    detail screen css

--- screens/ResultsShowScreen.js
+++ screens/ResultsShowScreen.js
@@ -21,18 +21,43 @@ export default function ResultsShowScreen({ route }) {
     }
 
     return (
-        <View>
-            <Text>{result.name}</Text>
-            <Text>{result.phone}</Text>
+        <View style={styles.container}>
+            <Text style={styles.name}>{result.name}</Text>
+            <Text style={styles.rating}>{result.rating} ★ Rating</Text>
+            <Text style={styles.phone}>Phone: {result.phone}</Text>
             <FlatList
+                horizontal
                 data={result.photos}
                 renderItem={({ item }) => {
-                    return <Image style={{ height: 100, width: 100 }} source={{ uri: item }} />
+                    return <Image style={styles.image} source={{ uri: item }} />
                 }
                 }
             />
         </View>
     )
-
-    const styles = StyleSheet.create({})
 }
+const styles = StyleSheet.create({
+    container:{
+        marginTop:10
+    },
+    image: {
+        height: 180,
+        width: 300,
+        margin: 10,
+        borderRadius: 20
+    },
+    name: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginLeft:10
+    },
+    phone:{
+        fontSize:15,
+        marginLeft:10
+    },
+    rating: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginLeft:10
+    },
+})
